6625541e3baeef018414c0088b03fd1ff0ec2032
Cem <cem.gencer.35@gmail.com>
2023-01-07 18:05:24 +0300

    config file changed

--- src/config/index.js
+++ src/config/index.js
@@ -1,38 +1,56 @@
 import { readFileSync } from "fs"
 
-export const SESSION_SECRET =
-	process.env.NODE_ENV == "production"
-		? readFileSync("../run/secrets/session_secret", "utf-8")
-		: "secret"
-export const DB_URL =
-	process.env.NODE_ENV == "production"
-		? readFileSync("../run/secrets/db_url", "utf-8")
+export const SESSION_SECRET = setVarAccordingToEnv(
+	"../run/secrets/session_secret",
+	"secret"
+)
+export const DB_URL = setVarAccordingToMultipleEnv(
+	"../run/secrets/db_url",
+	"localhost",
+	"auth-db"
+)
+export const DB_PORT = parseInt(
+	setVarAccordingToEnv("../run/secrets/db_port", 5432)
+)
+export const DB_USERNAME = setVarAccordingToEnv(
+	"../run/secrets/db_username",
+	"postgres"
+)
+export const DB_PASSWORD = setVarAccordingToEnv(
+	"../run/secrets/db_password",
+	"postgres"
+)
+export const DB_NAME = setVarAccordingToEnv("../run/secrets/db_name", "auth")
+export const REDIS_URL = setVarAccordingToMultipleEnv(
+	"../run/secrets/redis_url",
+	"localhost",
+	"redis"
+)
+export const REDIS_PORT = parseInt(
+	setVarAccordingToEnv("../run/secrets/redis_port", 6379)
+)
+
+/**
+ * @param {string} path File path
+ * @param {string | number} alternative Value to be used if file does not exists
+ * @return {string | number}
+ */
+function setVarAccordingToEnv(path, alternative) {
+	return process.env.NODE_ENV == "production"
+		? readFileSync(path, "utf-8")
+		: alternative
+}
+
+/**
+ * @param {string} path File path
+ * @param {string | number} fAlternative First alternative to be used if file does not exists
+ * @param {string | number} sAlternative Second alternative to be used if file does not exists
+ * @return {string | number}
+ */
+function setVarAccordingToMultipleEnv(path, fAlternative, sAlternative) {
+	return process.env.NODE_ENV == "production"
+		? readFileSync(path, "utf-8")
 		: process.env.NODE_ENV == "ghtest"
-		? "localhost"
-		: "auth-db"
-export const DB_PORT =
-	process.env.NODE_ENV == "production"
-		? parseInt(readFileSync("../run/secrets/db_port", "utf-8"))
-		: 5432
-export const DB_USERNAME =
-	process.env.NODE_ENV == "production"
-		? readFileSync("../run/secrets/db_username", "utf-8")
-		: "postgres"
-export const DB_PASSWORD =
-	process.env.NODE_ENV == "production"
-		? readFileSync("../run/secrets/db_password", "utf-8")
-		: "postgres"
-export const DB_NAME =
-	process.env.NODE_ENV == "production"
-		? readFileSync("../run/secrets/db_name", "utf-8")
-		: "auth"
-export const REDIS_URL =
-	process.env.NODE_ENV == "production"
-		? readFileSync("../run/secrets/redis_url", "utf-8")
-		: process.env.NODE_ENV == "ghtest"
-		? "localhost"
-		: "redis"
-export const REDIS_PORT =
-	process.env.NODE_ENV == "production"
-		? parseInt(readFileSync("../run/secrets/redis_port", "utf-8"))
-		: 6379
+		? fAlternative
+		: sAlternative
+}
